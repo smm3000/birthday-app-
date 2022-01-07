f5aa07b8f2ebc8bafd4f0862479fb4a59658354c
samir <s-malik@live.co.uk>
2022-01-07 11:17:58 +0000

    Enter Name & Birthday features added

---
+++ spec/features/features_spec.rb
@@ -0,0 +1,12 @@
+feature 'Enter Name & Birthday' do
+    scenario 'Ask for name' do 
+        visit ('/')
+        expect(page).to have_content ("What is your name?")
+    end
+
+    scenario 'Ask for Birthday' do
+        visit ('/')
+        expect(page).to have_content ("What is your birthday?")
+    end
+end 
+
