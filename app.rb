f9eaeb209863a5b8adf27947df44c83e3b491d39
samir <s-malik@live.co.uk>
2022-01-10 12:32:48 +0000

    Basic Message and set-up complete

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'sinatra/base'
 require 'sinatra/reloader'
 
 class Birthday < Sinatra::Base 
+    enable :sessions
     configure :development do 
         register Sinatra::Reloader 
     end 
@@ -9,6 +10,18 @@ class Birthday < Sinatra::Base
     get '/' do
         erb :index
     end
+
+    post '/info' do
+        session[:name] = params[:name]
+        session[:birthday] = params[:birthday]
+        redirect '/message'
+    end 
+
+    get '/message' do
+        @name = session[:name]
+        @birthday = session[:birthday]
+        erb :message
+    end 
 
     run! if app_file == $0
 end 
---
+++ lib/birth_date_spec.rb
@@ -0,0 +1,7 @@
+require 'birth_date'
+
+describe BirthDate do
+    it 'tests whether the correct date is returned' do
+    end 
+
+end 
