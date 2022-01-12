3a405e2d6799b4c99fe6fa2d1c0599dc08f313bc
samir <s-malik@live.co.uk>
2022-01-12 13:51:50 +0000

    Updated version - need to debug app

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
+require './lib/birth_date'
 
 class Birthday < Sinatra::Base 
     enable :sessions
@@ -13,17 +14,20 @@ class Birthday < Sinatra::Base
 
     post '/info' do
         session[:name] = params[:name]
-        session[:birthday] = params[:birthday]
+        session[:birthday_day] = params[:birthday_day]
+        session[:birthday_month] = params[:birthday_month]
         redirect '/message'
     end 
 
     get '/message' do
         @name = session[:name]
-        @birthday = session[:birthday]
+        @day = session[:birthday_day]
+        @month = session[:birthday_month]
+        @BirthDate = BirthDate.new(@day,@month)
         erb :message
     end 
 
-    
+
 
     run! if app_file == $0
 end 
