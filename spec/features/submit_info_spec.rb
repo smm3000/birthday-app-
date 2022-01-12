aff592b76868237c1a495b6c915120424ef51c19
samir <s-malik@live.co.uk>
2022-01-12 18:01:03 +0000

    Working with entering text as inputs

--- spec/features/submit_info_spec.rb
+++ spec/features/submit_info_spec.rb
@@ -2,7 +2,8 @@ feature 'Submitting Information' do
     scenario 'submit name and birthday' do
         visit ('/')
         fill_in :name, with: 'JimBob'
-        fill_in :birthday, with: '11/01/1999'
+        fill_in :birthday_day, with: 13
+        fill_in :birthday_month, with: 1
         click_on 'Submit'
         expect(page).to have_content 'JimBob, your birthday is 1 day away'
     end 
